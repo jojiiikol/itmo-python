e749704e1e21c220e2f52866cfc7868f0cf2f9db
jojiiikol <jojiiikol@gmail.com>
2026-09-22 23:15:12 +0300

    add comprasion

--- task/docs/scripts/generate_data.py
+++ task/docs/scripts/generate_data.py
@@ -5,9 +5,9 @@ import pandas as pd
 
 PATH = Path(__file__).resolve().parent.parent.parent / "data" / "for_graphics.csv"
 
-x_min = -200
-x_max = 10
-N = 500
+x_min = -5
+x_max = 5
+N = 10
 
 def generate():
     x_list = [x_min + i * (x_max - x_min) / (N - 1) for i in range(N)]
